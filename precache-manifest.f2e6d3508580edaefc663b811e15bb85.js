e29da076cb99eb232eb59c2f39d1b5c4738235eb
kruanurak <akhophadung@gmail.com>
2020-05-09 11:21:43 +0700

    Updates

---
+++ precache-manifest.f2e6d3508580edaefc663b811e15bb85.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40901c735415b8412b7414ec07d0ff0c",
+    "url": "/makejustone/index.html"
+  },
+  {
+    "revision": "d668dad207667aebeefc",
+    "url": "/makejustone/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "c1175b0a37d6b9f2d452",
+    "url": "/makejustone/static/css/main.fd0282ce.chunk.css"
+  },
+  {
+    "revision": "d668dad207667aebeefc",
+    "url": "/makejustone/static/js/2.574701d8.chunk.js"
+  },
+  {
+    "revision": "abe95fe52576d1d6086919e537132919",
+    "url": "/makejustone/static/js/2.574701d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1175b0a37d6b9f2d452",
+    "url": "/makejustone/static/js/main.6c574780.chunk.js"
+  },
+  {
+    "revision": "2b5fb7f68ed28f910f72",
+    "url": "/makejustone/static/js/runtime-main.8ea9b22c.js"
+  },
+  {
+    "revision": "f69fdd6858deaa1e307594ad974d3979",
+    "url": "/makejustone/static/media/ChakraPetch-Regular.f69fdd68.ttf"
+  },
+  {
+    "revision": "9bec958d1bdbaf4f03bdf23557b4122f",
+    "url": "/makejustone/static/media/K2D-Regular.9bec958d.ttf"
+  },
+  {
+    "revision": "a82c4c3bfe6cd8a9d57597071772d0c7",
+    "url": "/makejustone/static/media/Kanit-Regular.a82c4c3b.ttf"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/makejustone/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "16b4ce72cf30da14d83d5e3981d3113c",
+    "url": "/makejustone/static/media/Prompt-Regular.16b4ce72.ttf"
+  },
+  {
+    "revision": "49d082d0164cdf471f5e00b1fddb89f9",
+    "url": "/makejustone/static/media/Sarabun-Regular.49d082d0.ttf"
+  },
+  {
+    "revision": "c8039f52b7d129caf9343179dd6aff0f",
+    "url": "/makejustone/static/media/chome_ic.c8039f52.png"
+  },
+  {
+    "revision": "7d089fc9234caa2fa145ac9b54198226",
+    "url": "/makejustone/static/media/cover01.7d089fc9.jpg"
+  },
+  {
+    "revision": "0a215f0fb1c3515c13c02ad2496c130d",
+    "url": "/makejustone/static/media/cover02.0a215f0f.jpg"
+  },
+  {
+    "revision": "daf76df9804539c33a88b3204dcdae8d",
+    "url": "/makejustone/static/media/cover03.daf76df9.jpg"
+  },
+  {
+    "revision": "a2e339cf129285570639cb5fea4dd4ad",
+    "url": "/makejustone/static/media/cover04.a2e339cf.jpg"
+  },
+  {
+    "revision": "08c426463826eec7725e6cd99b2764be",
+    "url": "/makejustone/static/media/cover05.08c42646.jpg"
+  },
+  {
+    "revision": "6d2245daada70905c8f9a1a27d2e93db",
+    "url": "/makejustone/static/media/cover06.6d2245da.jpg"
+  },
+  {
+    "revision": "93a784892541adeb05595542167caa86",
+    "url": "/makejustone/static/media/cover11.93a78489.jpg"
+  },
+  {
+    "revision": "050dce8d9580375601b5d1ab7745d654",
+    "url": "/makejustone/static/media/cover11_bottomF.050dce8d.png"
+  },
+  {
+    "revision": "e24580adc5c378b5fe4ca424a8aef740",
+    "url": "/makejustone/static/media/cover11_middle.e24580ad.png"
+  },
+  {
+    "revision": "92292aee36e4d518b266493ef9ab0bee",
+    "url": "/makejustone/static/media/cover12.92292aee.jpg"
+  },
+  {
+    "revision": "6071c638e6c055fb0e77d1ccc794e684",
+    "url": "/makejustone/static/media/cover13.6071c638.jpg"
+  },
+  {
+    "revision": "850ad3b27d3293e8ae8eb118d3a4199e",
+    "url": "/makejustone/static/media/cover14.850ad3b2.jpg"
+  },
+  {
+    "revision": "ba57439a669a0a5f0ed3b5ff027e7b0a",
+    "url": "/makejustone/static/media/cover15.ba57439a.jpg"
+  },
+  {
+    "revision": "b7829e55e61f59b196d957d630da5f23",
+    "url": "/makejustone/static/media/cover16.b7829e55.jpg"
+  },
+  {
+    "revision": "074630b9ccc767f68b95e438e2fba1bd",
+    "url": "/makejustone/static/media/cover21.074630b9.png"
+  },
+  {
+    "revision": "c07ed96c9e2257dac8a14f4a39c3376f",
+    "url": "/makejustone/static/media/cover21_middle.c07ed96c.png"
+  },
+  {
+    "revision": "42d3894377aa21b2000dbce91242b1d2",
+    "url": "/makejustone/static/media/cover21_top.42d38943.png"
+  },
+  {
+    "revision": "e6e5c5733c226248a10fa192646927b4",
+    "url": "/makejustone/static/media/cover22.e6e5c573.png"
+  },
+  {
+    "revision": "ef98a23fe4bcf829f5381737919f55ff",
+    "url": "/makejustone/static/media/cover22_middle.ef98a23f.png"
+  },
+  {
+    "revision": "e4deb55c1f51810cc2521350b3f05906",
+    "url": "/makejustone/static/media/cover22_top.e4deb55c.png"
+  },
+  {
+    "revision": "31b630ad3a5c9426f39cca011478c0d1",
+    "url": "/makejustone/static/media/cover23.31b630ad.png"
+  },
+  {
+    "revision": "4527a634001646c37cbf92921b1a1014",
+    "url": "/makejustone/static/media/cover23_middle.4527a634.png"
+  },
+  {
+    "revision": "e15605666c83d20848a42954e42108c5",
+    "url": "/makejustone/static/media/cover23_top.e1560566.png"
+  },
+  {
+    "revision": "51e5f3fc9ef4b5f781a01f4e7b726592",
+    "url": "/makejustone/static/media/cover24.51e5f3fc.png"
+  },
+  {
+    "revision": "fa3b1c85f3e7a235896df15384b454ea",
+    "url": "/makejustone/static/media/cover24_middle.fa3b1c85.png"
+  },
+  {
+    "revision": "6457e188bc1103292f8acbe64e3ddde9",
+    "url": "/makejustone/static/media/cover24_top.6457e188.png"
+  },
+  {
+    "revision": "8d4530f679e92e601e70595a07009dd5",
+    "url": "/makejustone/static/media/cover25.8d4530f6.png"
+  },
+  {
+    "revision": "76fad4845b306d0d77a96cb858afd867",
+    "url": "/makejustone/static/media/cover25_middle.76fad484.png"
+  },
+  {
+    "revision": "d6d4a617537767d8d8875186e9a7368c",
+    "url": "/makejustone/static/media/cover25_top.d6d4a617.png"
+  },
+  {
+    "revision": "886fd1332e650a788ed785f71165012d",
+    "url": "/makejustone/static/media/cover26.886fd133.png"
+  },
+  {
+    "revision": "d7fda8659ea7b6e01141ad8f2322477a",
+    "url": "/makejustone/static/media/cover26_middle.d7fda865.png"
+  },
+  {
+    "revision": "882e8d8fe7343a776ebd822d0f90226a",
+    "url": "/makejustone/static/media/cover26_top.882e8d8f.png"
+  },
+  {
+    "revision": "5eb9e49988c6a49b86588718a03b9c5c",
+    "url": "/makejustone/static/media/forms.5eb9e499.jpg"
+  },
+  {
+    "revision": "e69e0207656175ac2ee7164ee0b29c16",
+    "url": "/makejustone/static/media/report1.e69e0207.jpg"
+  },
+  {
+    "revision": "c94e43e3a2ee597ea587c43675c9953d",
+    "url": "/makejustone/static/media/school_logo.c94e43e3.png"
+  },
+  {
+    "revision": "162633eed2206b6063fb9f672b76c0e8",
+    "url": "/makejustone/static/media/setup_1.162633ee.png"
+  },
+  {
+    "revision": "453e2eb9cbc638b6141ee0eef07b0d36",
+    "url": "/makejustone/static/media/sso_logo.453e2eb9.png"
+  },
+  {
+    "revision": "8b479d74077096e01d67f21b49ba8904",
+    "url": "/makejustone/static/media/tick.8b479d74.png"
+  }
+]);
